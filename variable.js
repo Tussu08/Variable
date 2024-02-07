bc9e9ede16c9413d08fec22bde1313a686599c14
Tussu08 <imtushar001@gmail.com>
2024-02-07 23:39:19 +0530

    Tussu

---
+++ variable.js
@@ -0,0 +1,70 @@
+// Ways To declare a variable
+// var, let, const
+
+let firstName = 'Tushar';
+let lastName = 'Kumar';
+
+console.log(firstName, lastName, age);
+
+var age = 30;
+console.log(age);
+// Naming Conventions
+// -only letters , Numbers, Underscore and dollar signs
+//- Cant start with aNumber
+
+//Multi-Word Formatting
+//firstName= CamelCase
+//first_name= Underscore
+//FirstName= pascalCase
+//firstname= lowercase
+age = 31;
+
+console.log(age);
+
+let score;
+score = 1
+
+console.log(score);
+
+// let can be used without initialisation
+// can change the value later
+
+const x = 100;
+
+console.log(x)
+
+// cant change const value and cannot init without a value
+
+let arr = [1, 2, 3, 4,];
+
+console.log(arr)
+
+arr = [1, 2, 3, 4, 5]
+
+console.log(arr)
+
+const arr1 = ['a', 'b',]
+
+console.log(arr1)
+
+arr1.push('c')
+
+console.log(arr1)
+
+const person = {
+    name: 'Tushar',
+};
+person.name = 'Kumar';
+person.email = 'imtusharo001@gmail.com'
+
+console.log(person)
+
+// declare multiple values at once 
+
+let a, b, c;
+
+const d = 10; e = 11, f = 12;
+
+console.log(a)
+
+a = 2
